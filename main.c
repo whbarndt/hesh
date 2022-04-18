37c814212452599f4cf37f82be2dd844366a1c81
Hunter Barndt <whbarndt@protonmail.com>
2022-04-17 22:42:38 -0800

    Added read_line and split_line w/comments. Working on reg expressions

--- main.c
+++ main.c
@@ -1,12 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <regex.h> only preinstalled on unix environments
+
+#define INPUT_BUFFER_SIZE 100
+#define TOKEN_BUFFER_SIZE 64
+#define HESH_TOK_DELIM " \t\r\n\a" // \t is tab, \r is carriage return, \n is line feed, \a is alert. (I thinks)
+
+//// Purpose: Function to split the full line of input sent to the shell
+//// Returns: An array of char arrays
+char **hesh_split_line(char *line)
+{
+  // Defines the variating buffersize, position variable for token array, a token (char array), and allocates an array of tokens (an array of arrays of characters)
+  int token_buffsize = TOKEN_BUFFER_SIZE;
+  int position = 0;
+  char **tokens = malloc(token_buffsize * sizeof(char*));
+  char *token;
+
+  // If tokens isn't allocated/malloc fails
+  if (!tokens) 
+  {
+    fprintf(stderr, "Error: malloc in hesh_split_line\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Creates an array of tokens by adding a null terminator after each whitespace deliminated token
+  // Returns the pointer to the first one in list.
+  token = strtok(line, HESH_TOK_DELIM);
+
+  // While the token char array isn't empty, store tokens into tokens array
+  while (token != NULL) 
+  {
+    // Stores a token into the tokens array 
+    tokens[position] = token;
+    position++;
+
+    // If our position has become bigger than what we allocated then expand it and reallocate
+    if (position >= token_buffsize) 
+    {
+      token_buffsize += TOKEN_BUFFER_SIZE;
+      tokens = realloc(tokens, token_buffsize * sizeof(char*));
+      
+      // If tokens isn't allocated/malloc fails
+      if (!tokens) 
+      {
+        fprintf(stderr, "Error: realloc in hesh_split_line\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+    token = strtok(NULL, HESH_TOK_DELIM);
+  }
+
+  // Sets the last position to be NULL
+  tokens[position] = NULL;
+  return tokens;
+}
+
+//// Purpose: Function to read the full line of input from stdin
+//// Returns: Char array
+char *hesh_read_line(void)
+{
+  // Defines the line and buffer size variable
+  char *line = NULL;
+  size_t buffsize = 0; // getline will allocate a buffer for us (for now)
+  
+  // Defining regular expression and the regex_t object
+  char* nono_chars = "[^a-zA-Z0-9\_\-\.\/]+";
+  regex_t re;
+  //regmatch_t* pmatch;
+
+  // Stores the white space deliminated line into the line variable
+  // If the getline returns -1 error in getline
+  if (getline(&line, &buffsize, stdin) == -1)
+  {
+    // If end-of-file indicator is set, exit 
+    if (feof(stdin)) 
+      exit(EXIT_SUCCESS);  
+    else  
+    {
+      perror("readline");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  // If the regular expression compilation fails, print error and exit
+  if(regcomp(&re, nono_chars, REG_EXTENDED) != 0){
+    fprintf(stderr, "Error: regcomp in hesh_read_line");
+    exit(EXIT_FAILURE);
+  }
+
+  // Execute finding regular expressions in the inputted line, store
+  //int status = regexec(&re, line, pmatch, NULL, 0); 
+  regfree(re);
+
+  return line;
+}
+
+//// Purpose: Function for main loop
+//// Returns: Void
 void hesh_loop()
 {
+    // Define variables for read line, array of arguments/commands, and the status excecuted command
     char *line;
     char **args;
     int status;
 
+    // Start of loop
     do {
         printf("$ ");
-        //line = hest_read_line();
+        line = hest_read_line();
         //args = hesh_split_line(line);
         //status = hesh_execute(args);
         wait();
@@ -19,7 +120,7 @@ int main(int argc, char **argv)
 {
   // Load config files, if any.
 
-  // Run command loop.
+  // Run shell loop.
   hesh_loop();
 
   // Perform any shutdown/cleanup.
